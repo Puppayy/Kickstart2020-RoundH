27c7ca93143102d1656f5f1af02b049118f7c2cd
Matthew Groholski <mattgroholski@yahoo.com>
2021-05-06 14:12:40 -0700

    V2.1

--- Problem 3/c.cpp
+++ Problem 3/c.cpp
@@ -4,8 +4,10 @@
 using namespace std;
 
 struct person {
-    int x,y;
+    long long x,y;
 };
+
+double avgX = 0, avgY = 0;
 
 void sort(person n[], int s);
 
@@ -17,14 +19,12 @@ int main () {
         int N;
         cin >> N;
 
-        double avgX = 0, avgY = 0;
-
         person people[N];
 
         for (int i = 0; i < N; i++) {
             person a;
             cin >> a.x >> a.y;
-            people[N] = a;
+            people[i] = a;
             avgX += a.x;
             avgY += a.y;
         }
@@ -39,12 +39,12 @@ int main () {
         int nAvgX = avgX - 1, pAvgX = avgX + 1;
         for (int i = 0; i < N; i++) {
             if (i == 0) {
-                steps += (avgX - people[i].x) + (avgY - people[i].y);
+                steps += abs((avgX - people[i].x)) + abs((avgY - people[i].y));
             } else if (people[i].x < avgX) {
-                steps += (nAvgX - people[i].x) + (avgY - people[i].y);
+                steps += abs((nAvgX - people[i].x)) + abs((avgY - people[i].y));
                 nAvgX--;
             } else {
-                steps += (pAvgX - people[i].x) + (avgY - people[i].y);
+                steps += abs((pAvgX - people[i].x)) + abs((avgY - people[i].y));
                 pAvgX++;
             }
         }
@@ -62,6 +62,51 @@ void sort(person n[], int s) {
     }
 
     //Sorts sub arrays
-    
+    int n1s = ceil(s/2);
+    person n1[n1s];
 
+    for (int i = 0; i < n1s; i++) {
+        n1[i] = n[i];
+    }
+
+    sort(n1, n1s);
+
+    int n2s = floor(s/2);
+    person n2[n2s];
+
+    for (int i = 0; i < n2s; i++) {
+        n2[i] = n[i + n1s];
+    }
+
+    sort(n2, n2s);
+
+    int i = 0, j = 0, k = 0;
+
+    while (i < n1s && j < n2s) {
+        double n1Distance = sqrt(pow(n1[i].x,2 - avgX) + pow(n1[i].y, 2 - avgY));
+        double n2Distance = sqrt(pow(n2[j].x,2 - avgX) + pow(n2[j].y, 2 - avgY));
+        if (n1Distance > n2Distance) {
+            n[k] = n2[j];
+            j++;
+            k++;
+        } else {
+            n[k] = n1[i];
+            i++;
+            k++;
+        } 
+    }
+
+    while (i < n1s) {
+        n[k] = n1[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2s) {
+        n[k] = n2[j];
+        j++;
+        k++;
+    }
+
+    return;
 }
--- Problem 3/test.cpp
+++ Problem 3/test.cpp
@@ -4,12 +4,5 @@
 using namespace std;
 
 int main () {
-    vector<int> i;
-    i.push_back(1);
-    i.push_back(2);
-    vector <int> j(3,10);
-    i = j;
-    for (int l = 0; l < i.size(); l++) {
-        cout << i[l];
-    }
+    
 }
